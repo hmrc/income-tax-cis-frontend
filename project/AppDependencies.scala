23840b1de0c229ead2b304df82b182aa992a2268
stephenheath101 <98922374+stephenheath101@users.noreply.github.com>
2025-03-13 17:02:49 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlay30Version = "8.6.0"
+  private val bootstrapPlay30Version = "8.5.0"
   private val mongoPlay30Version = "2.5.0"
 
   val jacksonAndPlayExclusions = Seq(
